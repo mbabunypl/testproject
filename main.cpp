90b804c69942a46f6cc23da115e1b733ccbd1737
mbabunypl <mahamudbabu@nypl.org>
2024-01-18 15:29:56 -0500

    Salutations

--- main.cpp
+++ main.cpp
@@ -4,45 +4,11 @@
 #include <memory>
 using namespace std;
 
-struct Task{
-    public:
-        Task(){
-            pq = queue<int>{};
-        }
 
-        void insert(int x){
-            pq.push(x);
-        }
-
-        void pop(){
-            pq.pop();
-        }
-
-        int front(){
-            return pq.front();
-        }
-
-        void print(){
-            if(!pq.empty()){
-                for(int i=0;i<pq.size();i++){
-                    cout << pq[i] << endl;
-                }
-            }
-        }
-
-        
-    private:
-        queue<int> pq;
+struct Node{
+    int x;
+    int y;
+    
 
     
 };
-
-
-
-
-int main(){
-
-    unique_ptr<Task> objptr = make_unique<Task>();
-    objptr->insert(32);
-
-}
